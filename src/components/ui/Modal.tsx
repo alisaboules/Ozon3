a6d3c19615ee44a7d511e4d18817c86ed536b7a6
Алиса <alisia.bajeeva247@gmail.com>
2026-08-07 21:41:39 +0500

    gg

--- src/components/ui/Modal.tsx
+++ src/components/ui/Modal.tsx
@@ -8,7 +8,7 @@ interface Props extends PropsWithChildren {
 
 export function Modal( { onClose, children, ref }: Props ) {
   return (
-    <div className="h-screen w-screen flex justify-center items-center bg-black/15 absolute top-0 left-0 z-100 fadeIn">
+    <div className="h-screen w-screen flex justify-center items-center bg-black/15 fixed top-0 left-0 z-100 fadeIn">
       <div ref={ref} className="bg-white p-5 rounded-3xl shadow w-sm relative">
         <button onClick={onClose} className="size-7 bg-zinc-200 hover:rotate-45 transition-all rounded-full flex justify-center items-center absolute top-3 right-3">
           <X className="opacity-60"/>
